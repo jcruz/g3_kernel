3ae92bae8678997ed0842c84f6da3df2810df264
Will Drewry <wad@chromium.org>
2012-02-09 12:08:39 -0600

    BACKPORT: ptrace,seccomp: Add PTRACE_SECCOMP support
    This change adds support for a new ptrace option, PTRACE_O_TRACESECCOMP,
    and a new return value for seccomp BPF programs, SECCOMP_RET_TRACE.
    
    When a tracer specifies the PTRACE_O_TRACESECCOMP ptrace option, the
    tracer will be notified, via PTRACE_EVENT_SECCOMP, for any syscall that
    results in a BPF program returning SECCOMP_RET_TRACE.  The 16-bit
    SECCOMP_RET_DATA mask of the BPF program return value will be passed as
    the ptrace_message and may be retrieved using PTRACE_GETEVENTMSG.
    
    If the subordinate process is not using seccomp filter, then no
    system call notifications will occur even if the option is specified.
    
    If there is no tracer with PTRACE_O_TRACESECCOMP when SECCOMP_RET_TRACE
    is returned, the system call will not be executed and an -ENOSYS errno
    will be returned to userspace.
    
    This change adds a dependency on the system call slow path.  Any future
    efforts to use the system call fast path for seccomp filter will need to
    address this restriction.
    
    v18: - rebase
         - comment fatal_signal check
         - acked-by
    v17: - ...
    v16: - update PT_TRACE_MASK to 0xbf4 so that STOP isn't clear on SETOPTIONS call (indan@nul.nu)
           [note PT_TRACE_MASK disappears in linux-next]
    v15: - add audit support for non-zero return codes
         - clean up style (indan@nul.nu)
    v14: - rebase/nochanges
    v13: - rebase on to 88ebdda6159ffc15699f204c33feb3e431bf9bdc
           (Brings back a change to ptrace.c and the masks.)
    v12: - rebase to linux-next
         - use ptrace_event and update arch/Kconfig to mention slow-path dependency
         - drop all tracehook changes and inclusion (oleg@redhat.com)
    v11: - invert the logic to just make it a PTRACE_SYSCALL accelerator
           (indan@nul.nu)
    v10: - moved to PTRACE_O_SECCOMP / PT_TRACE_SECCOMP
    v9:  - n/a
    v8:  - guarded PTRACE_SECCOMP use with an ifdef
    v7:  - introduced
    
    Change-Id: I1a6b98cc3dad0be83f856279f850f718a9888b53
    Acked-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: Will Drewry <wad@chromium.org>
    
    BUG=chromium-os:27878
    TEST=none. Part of a much larger stack that really should merge together.
    
    Change-Id: I1ba0786303612deae64878f8a8396541a9553a89
    Reviewed-on: https://gerrit.chromium.org/gerrit/21368
    Reviewed-by: Will Drewry <wad@chromium.org>
    Tested-by: Will Drewry <wad@chromium.org>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -575,6 +575,9 @@ static int ptrace_setoptions(struct task_struct *child, unsigned long data)
 	flags |= (data << PT_OPT_FLAG_SHIFT);
 	child->ptrace = flags;
 
+	if (data & PTRACE_O_TRACESECCOMP)
+		child->ptrace |= PT_TRACE_SECCOMP;
+
 	return 0;
 }
 
