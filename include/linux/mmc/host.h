a5e360e484c7b01385813a914e3ae3030c391858
Asutosh Das <asutoshd@codeaurora.org>
2013-06-03 18:29:59 +0530

    mmc: core: add wakeup functionality to sdio cards
    This patch initializes wakeup source if the detected card
    is a sdio card and enables the wakeup capability.
    
    Platform drivers would have to invoke:
     * pm_wakeup_event on this card device to signal a wakeup
     * corresponding pm_relax have to be invoked
    
    CRs-Fixed: 585481
    Change-Id: Ic8d5c98073e8ed3f676eb42fc0ce1f13a11cb40f
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    
    Conflicts:
    	include/linux/mmc/host.h

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -224,6 +224,12 @@ enum mmc_sdcc_controller_index {
 };
 #endif
 
+enum dev_state {
+	DEV_SUSPENDING = 1,
+	DEV_SUSPENDED,
+	DEV_RESUMED,
+};
+
 struct mmc_host {
 	struct device		*parent;
 	struct device		class_dev;
@@ -448,6 +454,7 @@ struct mmc_host {
 		struct delayed_work work;
 		enum mmc_load	state;
 	} clk_scaling;
+	enum dev_state dev_status;
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
