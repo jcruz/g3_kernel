737e70bd4127a0c415c5767c1b494a70ba94385c
Paul Reioux <reioux@gmail.com>
2014-02-15 13:26:45 -0800

    cpufreq: intelliactive: fix race condition when unregistering input handlers
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -1426,6 +1426,8 @@ static int cpufreq_governor_intelliactive(struct cpufreq_policy *policy,
 		}
 
 		if (--active_count > 0) {
+			if (!policy->cpu)
+				input_unregister_handler(&interactive_input_handler);
 			mutex_unlock(&gov_lock);
 			return 0;
 		}
