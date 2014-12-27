8ec70bdaf50487074744f6aa38449daa1b6b6142
yank555-lu <yank555.lu@gmail.com>
2014-12-27 04:03:42 +0100

    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.6 (faux123/Yank555.lu)
    - autosleep hook removed, not working on shamu
    - autosleep and hybrid modes removed
    - panel mode is now default
    - debug output switchable in defconfig

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -11,7 +11,6 @@
 #include <linux/pm_wakeup.h>
 
 #ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
 #include <linux/syscalls.h>
 #endif
 
@@ -109,21 +108,13 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
-#ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (sleep)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
 #ifndef CONFIG_PM_SYNC_BEFORE_SUSPEND
 		printk(KERN_INFO "PM: Syncing filesystems ... ");
 		sys_sync();
 		printk("done.\n");
 #endif
-#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
-#ifdef CONFIG_POWERSUSPEND
-		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)
-		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);
-#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
