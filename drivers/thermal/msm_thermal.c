29ab13d3a858e8f6f75450b488006cd52a456b18
anarkia1976 <stefano.villa1976@gmail.com>
2015-02-15 00:36:23 +0100

    msm: thermal: add scheduled timer to thermal init

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -2258,7 +2258,7 @@ int __devinit msm_thermal_init(struct msm_thermal_data *pdata)
 		pr_err("%s: cannot register cpufreq notifier\n",
 			KBUILD_MODNAME);
 	INIT_DELAYED_WORK(&check_temp_work, check_temp);
-	schedule_delayed_work(&check_temp_work, 0);
+	schedule_delayed_work(&check_temp_work, msecs_to_jiffies(10000));
 
 	if (num_possible_cpus() > 1)
 		register_cpu_notifier(&msm_thermal_cpu_notifier);
