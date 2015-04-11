2e4cc7a4fef0961ee39338cf7d224d5522206769
Pranav Vashi <neobuddy89@gmail.com>
2015-04-12 02:38:09 +0530

    msm: limiter: Export tunables to header file
    [777jon: adapt for g3]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- arch/arm/mach-msm/msm_limiter.c
+++ arch/arm/mach-msm/msm_limiter.c
@@ -23,15 +23,10 @@
 #include <linux/lcd_notify.h>
 #endif
 
-#define MSM_CPUFREQ_LIMIT_MAJOR		3
-#define MSM_CPUFREQ_LIMIT_MINOR		5
+#include <soc/qcom/limiter.h>
 
-#define MSM_LIMIT			"msm_limiter"
-#define LIMITER_ENABLED			1
-#define DEFAULT_SUSPEND_DEFER_TIME	10
-#define DEFAULT_SUSPEND_FREQUENCY	1728000
-#define DEFAULT_RESUME_FREQUENCY	2457600
-#define DEFAULT_MIN_FREQUENCY		300000
+#define MSM_CPUFREQ_LIMIT_MAJOR		3
+#define MSM_CPUFREQ_LIMIT_MINOR		6
 
 static unsigned int debug = 0;
 module_param_named(debug_mask, debug, uint, 0644);
@@ -41,34 +36,6 @@ do { 				\
 	if (debug)		\
 		pr_info(msg);	\
 } while (0)
-
-static struct cpu_limit {
-	unsigned int limiter_enabled;
-	uint32_t suspend_max_freq;
-	uint32_t resume_max_freq[4];
-	uint32_t suspend_min_freq[4];
-	unsigned int suspended;
-	unsigned int suspend_defer_time;
-	struct delayed_work suspend_work;
-	struct work_struct resume_work;
-	struct mutex resume_suspend_mutex;
-	struct mutex msm_limiter_mutex[4];
-#ifndef CONFIG_POWERSUSPEND
-	struct notifier_block notif;
-#endif
-} limit = {
-	.limiter_enabled = LIMITER_ENABLED,
-	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
-	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
-	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
-	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
-};
 
 static struct workqueue_struct *limiter_wq;
 
---
+++ include/soc/qcom/limiter.h
@@ -0,0 +1,34 @@
+#define MSM_LIMIT			"msm_limiter"
+#define LIMITER_ENABLED			1
+#define DEFAULT_SUSPEND_DEFER_TIME	10 
+#define DEFAULT_SUSPEND_FREQUENCY	1728000
+#define DEFAULT_RESUME_FREQUENCY	2457600
+#define DEFAULT_MIN_FREQUENCY		300000
+
+static struct cpu_limit {
+	unsigned int limiter_enabled;
+	uint32_t suspend_max_freq;
+	uint32_t resume_max_freq[4];
+	uint32_t suspend_min_freq[4];
+	unsigned int suspended;
+	unsigned int suspend_defer_time;
+	struct delayed_work suspend_work;
+	struct work_struct resume_work;
+	struct mutex resume_suspend_mutex;
+	struct mutex msm_limiter_mutex[4];
+#ifndef CONFIG_POWERSUSPEND
+	struct notifier_block notif;
+#endif
+} limit = {
+	.limiter_enabled = LIMITER_ENABLED,
+	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
+	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
+	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
+	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
+	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
+	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
+};
