6f0ec702c9deebd4fa19bf2103390d0dd177222f
777jon <jcruzdeveloper@gmail.com>
2015-07-10 13:10:52 -0400

    cpufreq: cpu-boost: Upgrade to enhanced version
    from: https://github.com/neobuddy89/hammerhead/commit/aa9aa4b693afb673479c3e9cc2c4c9e1a2972324
    
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -298,12 +298,32 @@ static void run_boost_migration(unsigned int cpu)
 	put_online_cpus();
 }
 
+static void cpuboost_set_prio(unsigned int policy, unsigned int prio)
+{
+	struct sched_param param = { .sched_priority = prio };
+
+	sched_setscheduler(current, policy, &param);
+}
+
+static void cpuboost_park(unsigned int cpu)
+{
+	cpuboost_set_prio(SCHED_NORMAL, 0);
+}
+
+static void cpuboost_unpark(unsigned int cpu)
+{
+	cpuboost_set_prio(SCHED_FIFO, MAX_RT_PRIO - 1);
+}
+
 static struct smp_hotplug_thread cpuboost_threads = {
 	.store		= &thread,
 	.thread_should_run = boost_migration_should_run,
 	.thread_fn	= run_boost_migration,
 	.thread_comm	= "boost_sync/%u",
+	.park		= cpuboost_park,
+	.unpark		= cpuboost_unpark,
 };
+
 
 static int boost_migration_notify(struct notifier_block *nb,
 				unsigned long unused, void *arg)
@@ -485,6 +505,16 @@ static struct notifier_block __refdata cpu_nblk = {
         .notifier_call = cpuboost_cpu_callback,
 };
 
+static void __wakeup_boost(void)
+{
+	if (!wakeup_boost || !input_boost_enabled ||
+	     work_pending(&input_boost_work))
+		return;
+	pr_debug("Wakeup boost for display on event.\n");
+	queue_work(cpu_boost_wq, &input_boost_work);
+	last_input_time = ktime_to_us(ktime_get());
+}
+
 #ifdef CONFIG_LCD_NOTIFY
 static int lcd_notifier_callback(struct notifier_block *this,
 				unsigned long event, void *data)
@@ -495,12 +525,7 @@ static int lcd_notifier_callback(struct notifier_block *this,
 	case LCD_EVENT_OFF_START:
 		break;
 	case LCD_EVENT_ON_END:
-		if (!wakeup_boost || !input_boost_enabled ||
-		     work_pending(&input_boost_work))
-			break;
-		pr_debug("Wakeup boost for LCD on event.\n");
-		queue_work(cpu_boost_wq, &input_boost_work);
-		last_input_time = ktime_to_us(ktime_get());
+		__wakeup_boost();
 		break;
 	default:
 		break;
@@ -546,9 +571,8 @@ static int cpu_boost_init(void)
 
 #ifdef CONFIG_LCD_NOTIFY
 	notif.notifier_call = lcd_notifier_callback;
-	ret = lcd_register_client(&notif);
-        if (ret != 0)
-                pr_err("Failed to register hotplug LCD notifier callback.\n");
+	if (lcd_register_client(&notif))
+		pr_err("Failed to register hotplug LCD notifier callback.\n");
 #endif
 
 	return ret;
