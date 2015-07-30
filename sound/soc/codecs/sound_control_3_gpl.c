fd2923bdd2b0a940e9a3e0ae1b1713a357d7c573
777jon <jcruzdeveloper@gmail.com>
2015-07-30 12:27:44 -0400

    Sound Control: Set default Speaker gain to +6 on boot
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -43,7 +43,7 @@ int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 #define REG_SZ	25
 
 #ifdef CONFIG_MACH_LGE
-static int cached_regs[] = {-1, -1, -1, -1, 0, 0, 1, -1, -1, -1,
+static int cached_regs[] = {-1, -1, -1, -1, 0, 0, 6, -1, -1, -1,
 			-1, -1, -1, -1, -1, -1, 3, 3, -1, -1,
 			-1, -1, -1, -1, -1};
 #else
