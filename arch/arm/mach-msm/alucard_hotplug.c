7a4d132ee0009328e0469430b8082b2ccfa2552b
alucard24 <dmbaoh2@gmail.com>
2015-10-03 17:44:10 +0200

    alucard_hotplug: use WQ_HIGHPRI workqueue!
    Conflicts:
    	arch/arm/mach-msm/alucard_hotplug.c

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -50,6 +50,7 @@ static DEFINE_PER_CPU(struct hotplug_cpuinfo, od_hotplug_cpuinfo);
 static struct notifier_block notif;
 #endif
 static struct delayed_work alucard_hotplug_work;
+static struct workqueue_struct *alucard_hp_wq;
 
 static struct hotplug_tuners {
 	unsigned int hotplug_sampling_rate;
@@ -232,7 +233,7 @@ static void __ref hotplug_work_fn(struct work_struct *work)
 		delay -= jiffies % delay;
 	}
 
-	queue_delayed_work_on(BOOT_CPU, system_wq,
+	queue_delayed_work_on(BOOT_CPU, alucard_hp_wq,
 				&alucard_hotplug_work,
 				delay);
 }
@@ -309,7 +310,7 @@ static int alucard_hotplug_callback(struct notifier_block *nb,
 			unsigned long action, void *data)
 {
 	unsigned int cpu = (unsigned long)data;
-	struct hotplug_cpuinfo *pcpu_info;
+	struct hotplug_cpuinfo *pcpu_info = NULL;
 
 	switch (action) {
 	case CPU_ONLINE:
@@ -317,14 +318,10 @@ static int alucard_hotplug_callback(struct notifier_block *nb,
 		pcpu_info->prev_cpu_idle = get_cpu_idle_time(cpu,
 				&pcpu_info->prev_cpu_wall,
 				0);
-		break;
-	case CPU_STARTING:
-		pcpu_info = &per_cpu(od_hotplug_cpuinfo, cpu);
 		pcpu_info->cur_up_rate = 1;
-		break;
-	case CPU_DYING:
-		pcpu_info = &per_cpu(od_hotplug_cpuinfo, cpu);
 		pcpu_info->cur_down_rate = 1;
+		break;
+	default:
 		break;
 	}
 
@@ -362,8 +359,9 @@ static void hotplug_start(void)
 	if (num_online_cpus() > 1) {
 		delay -= jiffies % delay;
 	}
+
 	INIT_DELAYED_WORK_DEFERRABLE(&alucard_hotplug_work, hotplug_work_fn);
-	queue_delayed_work_on(BOOT_CPU, system_wq,
+	queue_delayed_work_on(BOOT_CPU, alucard_hp_wq,
 				&alucard_hotplug_work,
 				delay);
 
@@ -758,6 +756,12 @@ static int __init alucard_hotplug_init(void)
 	if (ret) {
 		printk(KERN_ERR "failed at(%d)\n", __LINE__);
 		return ret;
+	}
+
+	alucard_hp_wq = alloc_workqueue("alu_hp_wq", WQ_HIGHPRI, 0);
+	if (!alucard_hp_wq) {
+		printk(KERN_ERR "Failed to create alu_hp_wq workqueue\n");
+		return -EFAULT;
 	}
 
 	/* INITIALIZE PCPU VARS */
