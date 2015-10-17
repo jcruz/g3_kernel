a50cf470b62e6890fecab56a4e346505fe3621a7
777jon <jcruzdeveloper@gmail.com>
2015-10-17 08:12:05 -0700

    msm_limiter: support alucard touch boost
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -77,6 +77,30 @@ struct cpufreq_suspend_t {
 
 static DEFINE_PER_CPU(struct cpufreq_suspend_t, cpufreq_suspend);
 
+#if defined(CONFIG_MSM_LIMITER) && defined(CONFIG_ALUCARD_TOUCHSCREEN_BOOST)
+static unsigned int lower_limit_freq[NR_CPUS] = {0, 0, 0, 0};
+
+unsigned int get_cpu_min_lock(unsigned int cpu)
+{
+	if (cpu >= 0 && cpu < NR_CPUS)
+		return lower_limit_freq[cpu];
+	else
+		return 0;
+}
+EXPORT_SYMBOL(get_cpu_min_lock);
+
+void set_cpu_min_lock(unsigned int cpu, int freq)
+{
+	if (cpu >= 0 && cpu < NR_CPUS) {
+		if (freq <= 268800 || freq > 2880000)
+			lower_limit_freq[cpu] = 0;
+		else
+			lower_limit_freq[cpu] = freq;
+	}
+}
+EXPORT_SYMBOL(set_cpu_min_lock);
+#endif
+
 unsigned long msm_cpufreq_get_bw(void)
 {
 	return mem_bw[max_freq_index];
@@ -120,6 +144,23 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 	int saved_sched_rt_prio = -EINVAL;
 	struct cpufreq_freqs freqs;
 	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
+#if defined(CONFIG_MSM_LIMITER) && defined(CONFIG_ALUCARD_TOUCHSCREEN_BOOST)
+	unsigned int ll_freq = lower_limit_freq[policy->cpu];
+
+	if (ll_freq) {
+		unsigned int t_freq = new_freq;
+
+		if (ll_freq && new_freq < ll_freq)
+			t_freq = ll_freq;
+
+		new_freq = t_freq;
+
+		if (new_freq < policy->min)
+			new_freq = policy->min;
+		if (new_freq > policy->max)
+			new_freq = policy->max;
+	}
+#endif
 
 	freqs.old = policy->cur;
 	freqs.new = new_freq;
