de974fd86ef41c7ffd444e2d166d3c0e0b9696bf
Hendrik Hagendorn <git@finnq.de>
2015-11-15 21:24:07 +0100

    g3: Use interruptible waits to fix high load average
    Based on @cyanogen's commit this replaces uninterruptible waits in
    worker threads with their interruptible versions to bring the load
    back down to zero at idle.
    
    To make sure that the waits can never be skipped by interrupts, the
    return values are checked and jumped back when the event has not yet
    finished.
    
    Change-Id: I14bad4af135b1594e235e2252cb484cb34d91054
    
    Conflicts:
    	arch/arm/mach-msm/mpm-of.c
    	arch/arm/mach-msm/rpm-smd.c
    	drivers/cpufreq/cpufreq_ondemand.c
    	drivers/slimbus/slim-msm-ngd.c
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1944,14 +1944,10 @@ static int __mdss_fb_display_thread(void *data)
 				mfd->index);
 
 	while (1) {
-		ret = wait_event_interruptible(mfd->commit_wait_q,
+		if (wait_event_interruptible(mfd->commit_wait_q,
 				(atomic_read(&mfd->commits_pending) ||
-				 kthread_should_stop()));
-
-		if (ret) {
-			pr_info("%s: interrupted", __func__);
+				 kthread_should_stop())) != 0)
 			continue;
-		}
 
 		if (kthread_should_stop())
 			break;
